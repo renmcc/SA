c4b5d225dd47758d1eb9e3ed00502a8516a42ed5
ren_mcc <ren_mcc@foxmail.com>
2020-06-16 22:12:27 +0800

    服务器列表页完成

--- backend/apps/cmdb/models.py
+++ backend/apps/cmdb/models.py
@@ -1,33 +1,17 @@
 from django.db import models
+from datetime import datetime
+from project.models import Project, ProjectRole, ProjectArea, ProjectRegion
+
 
 # Create your models here.
 
 
 class server(models.Model):
-    AREA = (
-        ('0', '未分配'),
-        ('101', '测试大区'),
-        ('102', '提审大区'),
-        ('103', '预发布大区'),
-        ('104', '正式大区')
-    )
-    PROJECT = (
-        ('未分配','未分配'),
-        ('王者荣耀','王者荣耀'),
-        ('和平精英','和平精英')
-    )
-    ROLE = (
-        ('未分配', '未分配'),
-        ('login', 'login'),
-        ('idip', 'idip'),
-        ('game', 'game'),
-        ('match', 'match'),
-        ('battlematch', 'battlematch'),
-        ('battle', 'battle'),
-    )
-    area = models.CharField('大区', max_length=50, choices=AREA, default='0', help_text='大区')
-    project = models.CharField('项目', max_length=50, choices=PROJECT, default='空闲', help_text='项目')
-    role = models.CharField('功能', max_length=50, choices=ROLE, default='空闲', help_text='功能')
+    region = models.ForeignKey(ProjectRegion, on_delete=models.SET_NULL, blank=True, null=True, related_name='server_region', verbose_name="区域", help_text='区域')
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, blank=True, null=True, related_name='server_project', verbose_name="项目", help_text='项目')
+    area = models.ForeignKey(ProjectArea, on_delete=models.SET_NULL,blank=True, null=True, related_name='server_area',verbose_name="大区", help_text='所属大区')
+    role = models.ManyToManyField(ProjectRole, blank=True, null=True, related_name='server_role', verbose_name="功能", help_text='功能')
+    # role = models.ForeignKey(ProjectRole, on_delete=models.SET_NULL, related_name='server_role', verbose_name="功能", help_text='功能', default=1)
     hostname = models.CharField('主机名', max_length=200, help_text='主机名',blank=True)
     public_ip = models.GenericIPAddressField('外网IP', help_text='外网IP',blank=True, null=True)
     private_ip = models.GenericIPAddressField('内网IP', unique=True, help_text='内网IP')
@@ -35,9 +19,10 @@ class server(models.Model):
     cpu = models.CharField("CPU信息", max_length=250, default=None, help_text="CPU信息",blank=True)
     memory = models.CharField("内存信息", max_length=100, default=None, help_text="内存信息",blank=True)
     disk = models.CharField("硬盘信息", max_length=300, null=True, help_text="硬盘信息", blank=True)
-    status = models.BooleanField('启用', default=False, help_text='是否启用')
+    status = models.BooleanField('启用', default=True, help_text='是否启用')
     remark = models.TextField("备注", null=True, help_text="备注",blank=True)
-    update_time = models.DateTimeField('更新时间', auto_now=True, help_text='更新时间')
+    add_time = models.DateTimeField(default=datetime.now, help_text='添加时间', verbose_name="添加时间")
+    update_time = models.DateTimeField(auto_now=True, help_text='更新时间', verbose_name='更新时间')
 
     def __str__(self):
         return self.private_ip
@@ -46,3 +31,20 @@ class server(models.Model):
         ordering = ('id',)
         verbose_name = '服务器列表'
         verbose_name_plural = '服务器列表'
+
+    @property
+    def regionInfo(self):
+        return {'id': self.region.id, 'name': self.region.name} if self.region else {'id': '', 'name': ''}
+    @property
+    def projectInfo(self):
+        return {'id': self.project.id, 'name': self.project.name} if self.project else {'id': '', 'name': ''}
+
+    @property
+    def areaInfo(self):
+        return {'id': self.area.id, 'name': self.area.name} if self.area else {'id': '', 'name': ''}
+
+    @property
+    def roleInfo(self):
+        serverObj = server.objects.get(id=self.pk)
+        role = serverObj.role.all()
+        return [x.name for x in role if role]
